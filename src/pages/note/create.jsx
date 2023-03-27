a93708bbfb15fcd3c6d9f3ba38b671d074474fa1
N-eeraj <neeraj7rajeena@gmail.com>
2023-03-27 22:02:02 +0530

    Save note API integration. Added word wrap for text component

--- src/pages/note/create.jsx
+++ src/pages/note/create.jsx
@@ -1,18 +1,47 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import Note from '@components/note'
 import Button from '@components/UI/Button'
 
+import { notEmptyValidator } from '@validators'
+
+import { toast } from '@toast'
+import useAxios from '@hooks/useAxios'
+
 import '@styles/pages/create.scss'
 
 const Create = () => {
+    const navigate = useNavigate()
+
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [loading, setLoading] = useState(false)
 
-    const saveNote = () => {
-        console.log(title)
-        console.log(body)
+    const handleSavedNote = ({ message }) => {
+        toast.success(message)
+        navigate('/')
+    }
+    
+    const validateInputs = () => {
+        try {
+            notEmptyValidator(title, 'Title')
+            notEmptyValidator(body, 'Body')
+
+            useAxios({
+                method: 'post',
+                url: '/note/create',
+                body: {
+                    title,
+                    body
+                },
+                setLoading,
+                successCallBack: handleSavedNote
+            })
+        }
+        catch (err) {
+            toast.error(err)
+        }
     }
 
     return (
@@ -23,7 +52,7 @@ const Create = () => {
 
                 <Button
                     text='Save'
-                    buttonClick={ saveNote }
+                    buttonClick={ validateInputs }
                     loading={ loading }
                     className='save-button' />
         </Note>
