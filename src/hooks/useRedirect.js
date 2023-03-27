f5c8cf2e2ec0183577d3953ca5bd8f60f271f6f2
N-eeraj <neeraj7rajeena@gmail.com>
2023-03-27 19:46:56 +0530

    Fix redirect issue on refresh in useRedirect hook

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,16 +1,22 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 
 const useRedirect = () => {
     const navigate = useNavigate()
+    const location = useLocation()
+
     useEffect(() => {
         document.addEventListener('redirect', ({ detail }) => {
             navigate(detail)
         })
 
         const authToken = localStorage.getItem('authToken')
-        navigate(authToken ? '/' : '/login')
+
+        if (['/login', '/register'].includes(location.pathname) && authToken)
+            navigate('/')
+        else if (!(authToken || ['/login', '/register'].includes(location.pathname)))
+            navigate('/login')
 
         return () => {
             document.removeEventListener('redirect', null)
